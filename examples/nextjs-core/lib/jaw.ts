e197e2b9c7f4ed4ac885230f2f1a480c1fbc79bb
AngeloAyranji <angeloayranji@gmail.com>
2026-02-24 17:35:56 +0200

    fix(nextjs-core): sponsor transaction

--- examples/nextjs-core/lib/jaw.ts
+++ examples/nextjs-core/lib/jaw.ts
@@ -6,11 +6,4 @@ export const jaw = JAW.create({
   ens: process.env.NEXT_PUBLIC_ENS_DOMAIN,
   defaultChainId: 84532,
   preference: { showTestnets: true },
-  paymasters: process.env.NEXT_PUBLIC_PIMLICO_API_KEY
-    ? {
-        8453: {
-          url: `https://api.pimlico.io/v2/8453/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`,
-        },
-      }
-    : undefined,
 });
